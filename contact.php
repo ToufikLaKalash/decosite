c1234860fc233be699c8d8dc98ac8eac8b5c0438
ToufikLaKalash <olivier.gisselmann@ynov.com>
2018-10-17 11:48:49 +0200

    Ajout des menus complets, des placeholder de presentation/galerie et du formulaire de contact

--- contact.php
+++ contact.php
@@ -7,14 +7,20 @@
   <body>
     <nav id="menu">
         <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Présentation</a></li>
-            <li><a href="#">Galerie</a></li>
-            <li><a href="#">Contact</a></li>
+          <li><a href="home.html">Accueil</a></li>
+          <li><a href="presentation.html">Présentation</a></li>
+          <li><a href="galerie.html">Galerie</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
     </nav>
     <main>
-        
+        <form class="contact" action="contact.php" method="post">
+            <input type="text" name="nom" placeholder="Votre nom">
+            <input type="text" name="Prénom" placeholder="Votre prénom">
+            <input type="email" name="email" placeholder="Votre email">
+            <textarea name="message" rows="8" cols="80" placeholder="Votre message"></textarea>
+            <button type="submit" name="envoyer">Envoyer</button>
+        </form>
     </main>
     <footer>&copy; Copyright 2018</footer>
   </body>
